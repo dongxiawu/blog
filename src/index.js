1066cc4fb85677a2bb53578f20594c07e4432500
dongxia <dongxia.wdx@alibaba-inc.com>
2018-11-04 11:13:24 +0800

    引入 ant design

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 
 import Home from './pages/home';
 
+import 'antd/dist/antd.css';
+
 // 整个项目的入口，将 <Home> 渲染到 index.html 模板文件的 id 为 root 的标签下
 ReactDOM.render(
   <Home />,
--- src/pages/home/components/header/Header.jsx
+++ src/pages/home/components/header/Header.jsx
@@ -12,7 +12,6 @@ class Header extends Component {
     this.state = {
       current: 'mail',
     };
-    // this.handleClick = this.handleClick.bind(this);
   }
 
   componentDidMount() {
@@ -33,25 +32,16 @@ class Header extends Component {
           onClick={this.handleClick}
           selectedKeys={[this.state.current]}
           mode="horizontal"
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+          }}
         >
           <Menu.Item key="mail">
             <Icon type="mail" />Navigation One
           </Menu.Item>
-          <Menu.Item key="app" disabled>
+          <Menu.Item key="app">
             <Icon type="appstore" />Navigation Two
-          </Menu.Item>
-          <SubMenu title={<span><Icon type="setting" />Navigation Three - Submenu</span>}>
-            <MenuItemGroup title="Item 1">
-              <Menu.Item key="setting:1">Option 1</Menu.Item>
-              <Menu.Item key="setting:2">Option 2</Menu.Item>
-            </MenuItemGroup>
-            <MenuItemGroup title="Item 2">
-              <Menu.Item key="setting:3">Option 3</Menu.Item>
-              <Menu.Item key="setting:4">Option 4</Menu.Item>
-            </MenuItemGroup>
-          </SubMenu>
-          <Menu.Item key="alipay">
-            <a href="https://ant.design" target="_blank" rel="noopener noreferrer">Navigation Four - Link</a>
           </Menu.Item>
         </Menu>
       </div>
